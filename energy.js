d4db3882e06df4b144393cbafeb3a9e896454037
Matthew Dorey <1@m3m3n70.com>
2014-03-12 02:11:55 -0700

    fs;

--- energy.js
+++ energy.js
@@ -2,7 +2,8 @@ var http = require('http');
 var express = require('express');
 var accountSid = 'AC705e42b0f48c9dc4aa055dd830a816ad';
 var authToken = "dfec68a266acd8126f76127c86e30364";
-var client = require('twilio')(accountSid, authToken);
+var twilio = require('twilio');
+var client = new twilio.RestClient(accountSid, authToken);
 var app = express();
 
 app.get('/', function(req, res){
@@ -39,7 +40,7 @@ function timer() {
 	if (count == 0) {
 		console.log('count is zerooo');
 		client.sms.messages.create({
-    		body: "Matt TEST!!",
+    		body: "Rate your energy between 1 and 5:",
    			 to: "+14153172907",
    			 from: "+14155287545"
 		}, function(err, message) {
@@ -47,8 +48,6 @@ function timer() {
 		});
 		count = 30;
 	}
-
-	//show seconds here
 }
 
 var port = Number(process.env.PORT || 5000);
