dd60558ee275a2c2388ac823f831e76ab7e7429f
Matthew Dorey <1@m3m3n70.com>
2014-03-16 01:57:08 -0700

    multi users

--- energy.js
+++ energy.js
@@ -13,6 +13,8 @@ var redisURL = url.parse(process.env.REDISCLOUD_URL || 6379);
 var dbConnection = redis.createClient(redisURL.port, redisURL.hostname, {no_ready_check: true});
 dbConnection.auth(redisURL.auth.split(":")[1]);
 
+var myhash = "blah";
+
 app.use(express.urlencoded());
 
 app.get('/', function(req, res){
